f0e31983f9d5983214229e8a3b4f00dd2f9f6d44
Travis Falk <falk.travis@gmail.com>
2024-10-25 15:37:33 -0400

    Updates

---
+++ assets/name-your-variables-B5t5uR1C.js
@@ -0,0 +1,10 @@
+const e=`---\r
+title: "Name Your Variables"\r
+writedate: "10/25/2024"\r
+timestamp: "1:25 PM"\r
+description: "I see devs naming their variables 'x' or 'var' all the time. Why not? Isn't it easier to type out? Why should you spend the effort to give it a name thats three words camel case? It's time to grow up..."\r
+length: "3 min read"\r
+image: "/images/themomtest.jpg"\r
+alt: "The Mom Test"\r
+\r
+---`;export{e as default};
