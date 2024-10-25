68ac4c7f71f33a5fc1f213b86a9c94e6f6b8d91f
Travis Falk <falk.travis@gmail.com>
2024-10-24 23:42:46 -0400

    Updates

---
+++ assets/name-your-variables-DiTPiAz_.js
@@ -0,0 +1,10 @@
+const e=`---\r
+title: "Name Your Variables"\r
+writedate: "10/25/2024"\r
+timestamp: "1:25 PM"\r
+description: "I see devs naming their variables 'x' time and time again. Why not? Isn't it easier to type out? Why should you spend the effort to give it a three word camel case name? It's time you found out..."\r
+length: "3 min read"\r
+image: "/images/themomtest.jpg"\r
+alt: "The Mom Test"\r
+\r
+---`;export{e as default};
