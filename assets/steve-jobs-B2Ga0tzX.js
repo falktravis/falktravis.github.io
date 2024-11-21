1d1e6e23cba55a803ad1731008a9670dfdcc6d8d
Travis Falk <falk.travis@gmail.com>
2024-11-21 15:01:11 -0500

    Updates

---
+++ assets/steve-jobs-B2Ga0tzX.js
@@ -0,0 +1,9 @@
+const e=`---\r
+title: "Steve Jobs"\r
+writedate: "11/21/2024"\r
+timestamp: "2:03 PM"\r
+description: "An absolutely encapsulating read. This book makes me want to read everything Walter Isaacson has ever written and generated a certian obsession with Jobs and his work. Here's my synopsis..."\r
+length: "3 min read"\r
+image: "/images/steve-jobs.jpg"\r
+alt: "Steve Jobs"\r
+---`;export{e as default};
---
+++ assets/steve-jobs-C_CWAe4W.js
@@ -0,0 +1,1 @@
+const e={title:"Steve Jobs",writedate:"11/21/2024",timestamp:"2:03 PM",description:"An absolutely encapsulating read. This book makes me want to read everything Walter Isaacson has ever written and generated a certian obsession with Jobs and his work. Here's my synopsis...",length:"3 min read",image:"/images/steve-jobs.jpg",alt:"Steve Jobs"};export{e as attributes};
