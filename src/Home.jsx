ac10d49d1353660170f34a1f4a0e7ad5ceaf1783
Travis Falk <falk.travis@gmail.com>
2024-11-26 12:18:10 -0500

    New Tech Stacks Game Section :)

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,19 +4,42 @@ import profileImg from '/images/profile.jpg'
 import { Link } from 'react-router-dom'
 
 //tech stack logos
-import gastbyLogo from '/images/gatsbyLogo.svg'
-import gcpLogo from '/images/gcpLogo.svg'
-import JSLogo from '/images/JSLogo.svg'
-import kerasLogo from '/images/KerasLogo.svg'
-import mongoLogo from '/images/mongoDBLogo.svg'
-import nodeLogo from '/images/nodeJSLogo.svg'
-import puppeteerLogo from '/images/puppeteerLogo.svg'
 import pythonLogo from '/images/pythonLogo.png'
+import tensorflowLogo from '/images/TensorFlowLogo.svg'
+import gcpLogo from '/images/gcpLogo.svg'
+import kerasLogo from '/images/KerasLogo.svg'
+
 import reactLogo from '/images/reactLogo.svg'
 import sassLogo from '/images/sassLogo.svg'
-import tensorflowLogo from '/images/TensorFlowLogo.svg'
+import nodeLogo from '/images/nodeJSLogo.svg'
+import mongoLogo from '/images/mongoDBLogo.svg'
+import gastbyLogo from '/images/gatsbyLogo.svg'
+import JSLogo from '/images/JSLogo.svg'
+import figmaLogo from '/images/FigmaLogo.svg'
+
+import puppeteerLogo from '/images/puppeteerLogo.svg'
+//node
+//js
+
 import unityLogo from '/images/unityLogo.svg'
 import CSharpLogo from '/images/CSharpLogo.svg'
+
+const techDict = {
+  'Python': pythonLogo,
+  'React': reactLogo,
+  'Node': nodeLogo,
+  'CSharp': CSharpLogo,
+  'Gastby': gastbyLogo,
+  'JS': JSLogo,
+  'Keras': kerasLogo,
+  'SASS': sassLogo,
+  'MongoDB': mongoLogo,
+  'Puppeteer': puppeteerLogo,
+  'Tensorflow': tensorflowLogo,
+  'Unity': unityLogo,
+  'GCP': gcpLogo,
+  'Figma': figmaLogo,
+}
 
 function generateRandomString(length) {
   const characters = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+}{][/<>';
@@ -133,6 +156,71 @@ export default function Home() {
 
     return () => clearInterval(interval);
   }, []);
+
+  // tech stack
+  const [isGame, setIsGame] = useState(null);
+
+  // no game - cards animation
+  const cardClick = (e) => {
+    e.preventDefault();
+    const card = e.currentTarget;
+    console.log(card);
+    card.classList.toggle('flipped');
+  }
+
+  // game stuff
+  const [currentSelection, setCurrentSelection] = useState([]);
+  const correctMatching = {
+    'Machine Learning': ['Python', 'Tensorflow', 'GCP', 'Keras'],
+    'Fullstack Web Development': ['React', 'SASS', 'Node', 'MongoDB', 'Gastby', 'JS', 'Figma'],
+    'Web Scraping': ['Puppeteer', 'Node', 'JS'],
+    'Game Development': ['Unity', 'CSharp']
+  }
+  const [currentStack, setCurrentStack] = useState('Machine Learning');
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isFinal, setIsFinal] = useState(false);
+
+  useEffect(() => {
+    console.log(currentSelection);
+    if(correctMatching[currentStack].length == currentSelection.length){
+      let correct = true;
+      correctMatching[currentStack].forEach((tech) => {
+        if(!currentSelection.includes(tech)){
+          correct = false;
+        }
+      });
+
+      if(correct){
+        let stackIndex = Object.keys(correctMatching).indexOf(currentStack);
+        if(stackIndex >= Object.keys(correctMatching).length - 1){ //end of stacks
+          setIsFinal(true);
+        }else{ //move to next stack
+          setIsSuccess(true);
+          setCurrentStack(Object.keys(correctMatching)[stackIndex + 1]);
+        }
+        setCurrentSelection([]);
+        document.querySelectorAll('.tech').forEach((tech) => {
+          tech.classList.remove('selected');
+        });
+      }
+    }
+  }, [currentSelection]);
+
+  useEffect(() => {
+    if(isSuccess){
+      setTimeout(() => {
+        setIsSuccess(false);
+      }, 1500);
+    }else if(isFinal){
+      setTimeout(() => {
+        setIsFinal(false);
+        setIsGame(false);
+        setCurrentStack('Machine Learning');
+      }, 3000);
+    }
+  }, [isSuccess, isFinal]);
+
+  //scroll animation
   
   return (
     <>  
@@ -181,19 +269,223 @@ export default function Home() {
             </p>
           </div>
           <div className="aboutContent">
-            <p>Nice to meet you! My name is Travis Falk. I am a 19 year old college student and product developer&#40;among other things&#41; living in Boston. I have been obsessed with building products for years, starting my development journey at 10 years old. This website is a collection of my previous experiences and projects, as well as my current interests and knowledge. Enjoy!</p>
             <img src={profileImg} alt="Profile Photo" />
+            <p>Nice to meet you! My name is Travis Falk. I am a 19 year old college student and product developer&#40;among other things&#41; living in Boston. I have been obsessed with building products for years, starting my development journey at 10 years old. This website is a collection of my previous experiences and projects, as well as my current interests and knowledge. Enjoy!</p>
           </div>
         </div>
-        <div className="techStack">
+        <div className="techStackContainer">
           <div className="head">
             <p className="pre-head">
               What can I do?
             </p>
             <h2>Tech Stacks</h2>
             <p className="sub-head">
-              Basic collection of my skills and technologies I am comfortable with
+              {isGame == null ? "Want to test your knowledge? Play a game to learn about my tech stacks or just skip to the information." : (isGame == true ? "Click on technologies to match them to the stack. When you get the correct match you will move to the next stack. Good luck!" : "Click on a card to learn more about my stacks!")}
             </p>
+          </div>
+          <div className="techStack">
+            {isGame == null ? 
+              <div className="gameDecision">
+                <button id='no' onClick={() => setIsGame(false)}>No Games</button>
+                <button id='yes' onClick={() => setIsGame(true)}>Play Game!</button> 
+              </div> : (isGame ?
+                  <div className="game">
+                    <button id='switch' onClick={() => setIsGame(false)}>Skip the Game</button>
+                    {isFinal ? <div className="win"><p id='success'>You Win!</p></div> : 
+                      <>{isSuccess ? <p id='success'>Correct!</p> : 
+                      <div className="instructions">
+                        <p id="instruction">Find technologies used for:</p>
+                        <p id='currentStack'>{currentStack}</p>
+                      </div>}
+                      <div className="options">
+                        {Object.entries(techDict).map(([tech, logo]) => (
+                          <button className="tech" key={tech} onClick={(e) => {
+                            e.preventDefault();
+                            if(!e.currentTarget.classList.contains('selected')){
+                              setCurrentSelection([...currentSelection, tech]);
+                            }else{
+                              setCurrentSelection(currentSelection.filter((selected) => selected !== tech));
+                            }
+                            e.currentTarget.classList.toggle('selected');
+                          }}>
+                            <img src={logo} alt={`${tech} logo`} />
+                            <p>{tech}</p>
+                          </button>
+                        ))}
+                      </div></>
+                    }
+                  </div> : 
+                  <div className="noGame">
+                    <button id='switch' onClick={() => setIsGame(true)}>Play the Game</button>
+                    <div onClick={cardClick} className="machineLearning">
+                      <div className="flipContainer">
+                        <div className="front">
+                          <h3>Machine Learning</h3>
+                          <p>Data manipulation, synthesis, retrieval, and analysis/prediction using machine learning and cloud computing platforms. There is a reason this complex tech stack is so popular.</p>
+                        </div>
+                        <div className="back">
+                          <div className="sectionContainer">
+                            <p>Language&#40;s&#41;:</p>
+                            <div className="language">
+                              <div className="tech">
+                                <img src={pythonLogo} alt="Python logo" />
+                                <p>Python</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="sectionContainer">
+                            <p>'Framework'&#40;s&#41;:</p>
+                            <div className="framework">
+                              <div className="tech">
+                                <img src={tensorflowLogo} alt="Tensorflow logo" />
+                                <p>Tensorflow</p>
+                              </div>
+                              <div className="tech">
+                                <img src={kerasLogo} alt="Keras logo" />
+                                <p>Keras</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="sectionContainer">
+                            <p>Other:</p>
+                            <div className="other">
+                              <div className="tech">
+                                <img src={gcpLogo} alt="GCP logo" />
+                                <p>GCP</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div onClick={cardClick} className="webDevelopment">
+                      <div className="flipContainer">
+                        <div className="front">
+                          <h3>Fullstack Website Development</h3>
+                          <p>A Classic, industry standard, fullstack development toolkit. I can do everything from beautiful design, to real time data updates, payment processing, and other integrations.</p>
+                        </div>
+                        <div className="back">
+                          <div className="sectionContainer">
+                            <p>Language&#40;s&#41;:</p>
+                            <div className="language">
+                              <div className="tech">
+                                <img src={JSLogo} alt="JavaScript logo" />
+                                <p>JavaScript</p>
+                              </div>
+                              <div className="tech">
+                                <img src={sassLogo} alt="SAAS logo" />
+                                <p>SAAS</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="sectionContainer">
+                            <p>'Framework'&#40;s&#41;:</p>
+                            <div className="framework">
+                              <div className="tech">
+                                <img src={nodeLogo} alt="NodeJS logo" />
+                                <p>NodeJS</p>
+                              </div>
+                              <div className="tech">
+                                <img src={reactLogo} alt="React logo" />
+                                <p>React</p>
+                              </div>
+                              <div className="tech">
+                                <img src={gastbyLogo} alt="Gatsby logo" />
+                                <p>Gatsby</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="sectionContainer">
+                            <p>Other:</p>
+                            <div className="other">
+                              <div className="tech">
+                                <img src={figmaLogo} alt="Figma logo" />
+                                <p>Figma</p>
+                              </div>
+                              <div className="tech">
+                                <img src={mongoLogo} alt="MongoDB logo" />
+                                <p>MongoDB</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div onClick={cardClick} className="webScraping">
+                      <div className="flipContainer">
+                        <div className="front">
+                          <h3>Web Scraping</h3>
+                          <p>Dissecting and reverse engineering websites to extract data with headless browser manipulation. This niche and extremely fun tech stack was my pet project through highschool.</p>
+                        </div>
+                        <div className="back">
+                          <div className="sectionContainer">
+                            <p>Language&#40;s&#41;:</p>
+                            <div className="language">
+                              <div className="tech">
+                                <img src={JSLogo} alt="JavaScript logo" />
+                                <p>JavaScript</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="sectionContainer">
+                            <p>'Framework'&#40;s&#41;:</p>
+                            <div className="framework">
+                              <div className="tech">
+                                <img src={nodeLogo} alt="NodeJS logo" />
+                                <p>NodeJS</p>
+                              </div>
+                              <div className="tech">
+                                <img src={puppeteerLogo} alt="Puppeteer logo" />
+                                <p>Puppeteer</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="sectionContainer">
+                            <p>Other:</p>
+                            <div className="other">
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div onClick={cardClick} className="gameDevelopment">
+                      <div className="flipContainer">
+                        <div className="front">
+                          <h3>Game Development</h3>
+                          <p>Dreaming up games using Unity and C#. A really basic tech stack, yet it remains one of my favorites to date.</p>
+                        </div>
+                        <div className="back">
+                          <div className="sectionContainer">
+                            <p>Language&#40;s&#41;:</p>
+                            <div className="language">
+                              <div className="tech">
+                                <img src={CSharpLogo} alt="CSharp logo" />
+                                <p>C#</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="sectionContainer">
+                            <p>'Framework'&#40;s&#41;:</p>
+                            <div className="framework">
+
+                            </div>
+                          </div>
+                          <div className="sectionContainer">
+                            <p>Other:</p>
+                            <div className="other">
+                              <div className="tech">
+                                <img src={unityLogo} alt="Unity logo" />
+                                <p>Unity</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              )
+            }
           </div>
         </div>
       </main>
